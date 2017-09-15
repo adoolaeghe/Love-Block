a1deb3ed9033a2b2069e6f3f0643c60003844fef
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-09-15 11:53:24 +0100

    config app3Js file

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
