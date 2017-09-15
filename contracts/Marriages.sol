09cd7ad5d962fe567d2d94e3ef4bd75e50dbf22a
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-09-15 17:34:37 +0100

    Clean up: delete commented out code.

--- contracts/Marriages.sol
+++ contracts/Marriages.sol
@@ -52,46 +52,4 @@ contract Marriages {
     marriages[marId].people.push(Person({_address:_person, firstName:firstName}));
     return true;
   }
-
-
-
-
-
-
-
-
-
-  /*struct Person {
-    address _address;
-    bytes32 firstName;
-    bytes32 middleName;
-    bytes32 familyName;
-    bytes32 dateOfBirth;
-    bytes32 placeOfBirth;
-    uint id;
-  }*/
-
-  /*Person[] public people;
-
-  function addPerson(bytes32 _firstName, bytes32 _middleName, bytes32 _familyName, bytes32 _dateOfBirth, bytes32 _placeOfBirth, uint _id) public returns (bool) {
-    people.length++;
-    people[people.length-1]._address = msg.sender;
-    people[people.length-1].firstName = _firstName;
-    people[people.length-1].middleName = _middleName;
-    people[people.length-1].familyName = _familyName;
-    people[people.length-1].dateOfBirth = _dateOfBirth;
-    people[people.length-1].placeOfBirth = _placeOfBirth;
-    people[people.length-1].id = _id;
-    return true;
-  }
-
-  function getPerson(uint256 _index) public returns (address, bytes32, bytes32, bytes32, bytes32, bytes32, uint) {
-    return (people[_index]._address,
-            people[_index].firstName,
-            people[_index].middleName,
-            people[_index].familyName,
-            people[_index].dateOfBirth,
-            people[_index].placeOfBirth,
-            people[_index].id);
-  }*/
 }
--- test/TestMarriages.sol
+++ test/TestMarriages.sol
@@ -53,36 +53,3 @@ contract TestMarriages {
     Assert.equal(marriages.marriageGetPersonAddress(marId, 0), addressJohn, "Test contract stores first person address.");
   }
 }
-
-/*contract TestMarriages {
-  Marriages marriages = Marriages(DeployedAddresses.Marriages());
-  bytes32 firstName = "John";
-  bytes32 middleName = "Spencer";
-  bytes32 familyName = "Smith";
-  bytes32 dateOfBirth = "19.02.1907";
-  bytes32 placeOfBirth = "Edinburgh";
-  uint id = 1287163912837;
-
-  function testContractAcceptsNewPerson() {
-    bool expectedStatus = marriages.addPerson(firstName, middleName, familyName, dateOfBirth, placeOfBirth, id);
-    Assert.equal(expectedStatus, true, "Contract accepts a new person.");
-  }
-
-  function testContractStoresNewPerson() {
-    var (returnedAddress,
-          returnedFirstName,
-          returnedMiddleName,
-          returnedFamilyname,
-          returnedDateOfBirth,
-          returnedPlaceOfBirth,
-          returnedId) = marriages.getPerson(0);
-
-    Assert.equal(returnedAddress, this, "Contract stores a new person address.");
-    Assert.equal(returnedFirstName, firstName, "Contract stores a new person first name.");
-    Assert.equal(returnedMiddleName, middleName, "Contract stores a new person middle name.");
-    Assert.equal(returnedFamilyname, familyName, "Contract stores a new person family name.");
-    Assert.equal(returnedDateOfBirth, dateOfBirth, "Contract stores a new person date of birth.");
-    Assert.equal(returnedPlaceOfBirth, placeOfBirth, "Contract stores a new person place of birth.");
-    Assert.equal(returnedId, id, "Contract stores a new person id number.");
-  }
-}*/
